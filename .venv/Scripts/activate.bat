0ddcff11e7d92b1c5beb968eb035b699a159da7f
TimmoTismo <44039129+TimmoTismo@users.noreply.github.com>
2023-06-16 20:23:34 +0100

    ... git push

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Timmo\OneDrive\Documents\Uni\Year 3\Dissertation\src\venv
+set VIRTUAL_ENV=C:\Users\Timmo\Documents\Workspaces\Diss-Rework\SEA\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
